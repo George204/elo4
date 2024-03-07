77d21af4c09072c53191329cbb57c93dd80364ba
George204 <hubert.lazowy@gmail.com>
2024-03-07 19:36:13 +0000

    incomplet

---
+++ public_html/zmienne.py
@@ -0,0 +1,66 @@
+klasy = {
+    '1a': 'o1', '1b': 'o2', '1c': 'o3', '1d': 'o4', '1e': 'o5', '1f': 'o6', '1g': 'o7', '1h': 'o8',
+    '2a': 'o9', '2b': 'o10', '2d': 'o11', '2f': 'o12', '2g': 'o13',
+    '3a': 'o14', '3b': 'o15', '3d': 'o16', '3f': 'o17', '3g': 'o18',
+    '4a': 'o19', '4b': 'o20', '4d': 'o21', '4e': 'o22', '4f': 'o23', '4g': 'o24'
+}
+klasy_v = [
+    ["1a","2a","3a","4a"],
+    ["1b","2b","3b","4b"],
+    ["1c"],
+    ["1d","2d","3d","4d"],
+    ["1e","4e"],
+    ["1f","2f","3f","4f"],
+    ["1g","2g","3g","4g"],
+    ["1h"],
+]
+nauczyciele = {
+    'Korońska': 'n1',
+    'Lasko-Janas': 'n2',
+    'Leciński': 'n3',
+    'Majchrzycka': 'n4',
+    'Tomaszewska L.': 'n5',
+    'Hrynyk': 'n6',
+    'Piątkowska': 'n7',
+    'Tabeau-Lijewska': 'n8',
+    'Taborek': 'n9',
+    'Gaczyńska': 'n10',
+    'Gorączniak': 'n11',
+    'Florek': 'n12',
+    'Jerzak': 'n13',
+    'Juskowiak': 'n14',
+    'Kąkol': 'n15',
+    'Paździor': 'n16',
+    'Skrzypek': 'n17',
+    'Burchardt': 'n18',
+    'Schramm': 'n19',
+    'Szulerowicz': 'n20',
+    'Żurkiewicz': 'n21',
+    'Andrzejczak': 'n22',
+    'Kodyniak': 'n23',
+    'Barabas': 'n24',
+    'Mikulska': 'n25',
+    'Elbanowska': 'n26',
+    'Gutorska': 'n27',
+    'Nienartowicz': 'n28',
+    'Nowak': 'n29',
+    'Dróżdż': 'n30',
+    'Wiesner': 'n31',
+    'Adamczyk': 'n32',
+    'Grochowalska': 'n33',
+    'Kasprzak': 'n34',
+    'Kozłowska': 'n35',
+    'Tomaszewska K.': 'n36',
+    'Dziadkiewicz': 'n37',
+    'Trepińska-Wojkiewicz': 'n38',
+    'Przykucki': 'n39',
+    'Andrzejewska': 'n40',
+    'Kalemba': 'n41',
+    'Kujawa': 'n42',
+    'Miś': 'n43'
+}
+nauczyciele_l =[]
+
+for nau in nauczyciele:
+    nauczyciele_l.append(nau)
+
