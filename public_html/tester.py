c65232e02c7394c998711ff9ceeb0812b3b2c5eb
George204 <97558179+George204@users.noreply.github.com>
2024-04-17 00:55:02 +0200

    tester

---
+++ public_html/tester.py
@@ -0,0 +1,17 @@
+import requests
+from zast2 import pobierz_przyciski, zrub_przyciski
+przyciski = zrub_przyciski(pobierz_przyciski())
+mego = {}
+for i in przyciski:
+    mego.update(i)
+for i in mego:
+    url = f"https://elo4.pl/plan/{i}%20{mego[i]}"
+    re = requests.get(url)
+    print(i,end=" ")
+    print(mego[i], end=" ")
+    if re.ok:
+        print("\033[32mok\033[0m")
+    else:
+        for i in range(20):
+            print("\033[31mporzażka!!!!!!!\033[0m")
+
